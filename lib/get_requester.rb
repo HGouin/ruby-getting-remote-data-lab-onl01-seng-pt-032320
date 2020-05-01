64c6162f3a77c3da8fdd7cf290e6d390d58ef8fd
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-05-01 04:03:48 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,4 +6,10 @@ class GetRequester
 def initialize
   @URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
 end
+
+def get_response_body
+end
+
+def parse_json
+end
 end
