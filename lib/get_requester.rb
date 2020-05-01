e106907e884bfb2281b141c16f155d6ff84bfb93
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-05-01 03:43:54 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,22 +3,4 @@ require 'net/http'
  require 'json'
 
 class GetRequester
-
-  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
-
-  def get_response_body
-    uri = URI.parse(@URL)
-    response = Net::HTTP.get_response(uri)
-    response.body
-  end
-
-  def parse_json
-    programs = JSON.parse(self.get_programs)
-  programs.collect do |program|
-    program["agency"]
-  end
 end
-
-end
-get_response_body = GetRequester.new
-puts programs.program_school.uniq
