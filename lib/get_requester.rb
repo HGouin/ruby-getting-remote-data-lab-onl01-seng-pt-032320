65919a94eefb2698728b4f1eb3976b02f101d3f4
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-05-01 03:49:26 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,4 +3,9 @@ require 'net/http'
  require 'json'
 
 class GetRequester
+
+url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+JSON.parse(response)
 end
